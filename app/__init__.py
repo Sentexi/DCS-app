b4bf83f5b59967d7c0312404843bad1cfbb13e15
Sentexi <communicationbel@gmail.com>
2025-05-25 20:40:15 +0200

    Added last active value

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 # app/__init__.py
 
-from flask import Flask
+from flask import Flask, request
 from .extensions import db, login_manager, migrate
+from flask_login import current_user
+from datetime import datetime
 
 def create_app(config_file=None):
     app = Flask(__name__)
@@ -35,5 +37,12 @@ def create_app(config_file=None):
     app.register_blueprint(profile_bp)
     
     print(app.url_map)
+    
+    @app.before_request
+    def update_last_seen():
+        # Only for authenticated users, and not for static/assets
+        if current_user.is_authenticated and not request.path.startswith('/static'):
+            current_user.last_seen = datetime.utcnow()
+            db.session.commit()
 
     return app
